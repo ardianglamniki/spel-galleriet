43118b909640ba6944a8942d19a1e93a5351de7a
Ardian Glamniki <ardian.glamniki@hotmail.com>
2022-07-24 17:02:03 +0200

    setup react-popper

--- src/components/Deal.jsx
+++ src/components/Deal.jsx
@@ -1,4 +1,36 @@
+import { placements } from '@popperjs/core';
+import { useState } from 'react';
+import { usePopper } from 'react-popper';
+import RedirectButton from './RedirectButton';
+
+// API
+import { getDealDetail } from '../api/getGameDeals'
+
+
 const Deal = ({ deal, isDeals }) => {
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'top-start',
+    modifiers: [
+      { name: 'arrow', options: { element: arrowElement } },
+      { name: 'eventListeners', options: { scroll: false } }
+    ]
+  });
+  const [isOpen, setIsOpen] = useState(false)
+  const [dealDetails, setDealDetails] = useState([])
+  const [cheapestPrice, setCheapestPrice] = useState(null)
+
+
+  const onButtonClick = async () => {
+    setIsOpen(!isOpen)
+    const response = await getDealDetail(deal.cheapestDealID)
+    setDealDetails(response.data.gameInfo)
+    setCheapestPrice(response.data.cheapestPrice.price)
+
+  }
+
 
   return (
     <div className='card mb-2 mx-2'>
@@ -28,14 +60,15 @@ const Deal = ({ deal, isDeals }) => {
           :
           <>
             <h5 className='card-title'>{deal.external}</h5>
-              <pre>
+            <pre>
               Nuvarande billigaste pris: {deal.cheapest}$
-              </pre>
+            </pre>
             <button
+              ref={setReferenceElement}
               className='btn btn-success'
-              onClick={() => window.open(`https://www.cheapshark.com/redirect?dealID=${deal.cheapestDealID}`)}
+              onClick={onButtonClick}
             >
-              Läs mer om spelet & försäljaren
+              Mer info
             </button>
           </>
         }
