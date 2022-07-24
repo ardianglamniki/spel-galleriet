e1141e0b1771d6b31cef7640ef9abe6f7467de62
Ardian Glamniki <ardian.glamniki@hotmail.com>
2022-07-24 16:50:58 +0200

    add function to fetch details and some refactoring

--- src/api/getGameDeals.js
+++ src/api/getGameDeals.js
@@ -1,21 +1,39 @@
 import axios from 'axios'
 
-export const getGameDeals = async () => {
-    const API_URL = 'https://www.cheapshark.com/api/1.0/deals'
-    const response = await axios.get(API_URL)
+let baseURL = 'https://www.cheapshark.com/api/1.0'
+export const fetchData = async (url) => {
+    try {
+        const response = await axios.get(url)
+        return response
+    }
+    catch (error) {
+        return error
+    }
+}
 
+const getGameDeals = async () => {
+    const response = await fetchData(baseURL + '/deals')
     const uniqueDeals = [... new Map(response.data.map(deal => [deal['title'], deal])).values()]
-
     return uniqueDeals
 }
 
-export const getGameDetailHistory = async (gameId) => {
-    const API_URL = `https://www.cheapshark.com/api/1.0/games?id=${gameId}`
+const getGameDetailHistory = async (gameId) => {
+    return fetchData(baseURL + `/games?id=${gameId}`)
+}
 
-    const response = await axios.get(API_URL)
-    console.log(response)
+const getGamesByTitle = async (title) => {
+    return fetchData(baseURL + `/games?title=${title}`)
+}
 
-    return response
+const getDealDetail = async (dealId) => {
+    return fetchData(baseURL + `/deals?id=${dealId}`)
+}
+
+export {
+    getGameDeals,
+    getGameDetailHistory,
+    getGamesByTitle,
+    getDealDetail
 }
 
 
